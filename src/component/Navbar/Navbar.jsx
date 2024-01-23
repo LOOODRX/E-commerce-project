20fd34bc7c8f121c5433705ff400a7282ae19d15
LOOODRX <220316534@aston.ac.uk>
2024-01-23 16:48:32 +0000

    Initial commit

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import Logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
+import { Link } from 'react-router-dom';
+
+
+const Navbar = () => {
+  const [menu,setMenu] = useState("shop")
+  return (
+    <div className='navbar'>
+      <div className="nav-logo">
+        <img src={Logo} alt="" />
+        <p>Book Store</p>
+      </div>
+
+      <ul className="nav-menu">
+      <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none'}} to='/'>SHOP</Link>{menu === "shop"?<hr/>:<></>}</li>
+      <li onClick={()=>{setMenu("men")}}><Link style={{textDecoration: 'none'}} to='/men'>MEN</Link>{menu === "men"?<hr/>:<></>}</li>
+      <li onClick={()=>{setMenu("women")}}><Link style={{textDecoration: 'none'}} to='/women'>WOMEN</Link>{menu === "women"?<hr/>:<></>}</li>
+      <li onClick={()=>{setMenu("kids")}}><Link style={{textDecoration: 'none'}} to='/kids'>KIDS</Link>{menu === "kids"?<hr/>:<></>}</li>
+      </ul>
+
+      <div className="nav-login-cart">
+        <Link to = '/login'><button className="login"> login </button></Link>
+        <Link to = '/cart'><img src={cart_icon} alt="" /></Link>
+        <div className="nav-cart-count"> 0 </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
