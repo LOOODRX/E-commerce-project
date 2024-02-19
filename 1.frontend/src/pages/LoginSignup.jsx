3ed463e09681692f6918706e839d8f881a12fbdb
LOOODRX <220316534@aston.ac.uk>
2024-02-19 11:17:15 +0000

    update frontend

--- 1.frontend/src/pages/LoginSignup.jsx
+++ 1.frontend/src/pages/LoginSignup.jsx
@@ -1,18 +1,75 @@
 import React from 'react'
 import './CSS/Loginsignup.css'
+import { useState } from 'react'
 
 const LoginSignup = () => {
+
+  const [state,setState] = useState("Login")
+  const [formData,setformData] = useState({
+    username:"",
+    password:"",
+    email:""
+  })
+
+  const changeHandler = (e) =>{
+    setformData({...formData,[e.target.name]:e.target.value})
+    
+  }
+
+  const login = async () => {
+    console.log("Login Function excuted",formData)
+    let responseData;
+    await fetch('http://localhost:4000/login',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-type':'application/json'
+      },
+      body: JSON.stringify(formData),
+    }).then((response)=>response.json()).then((data)=>responseData=data)
+  
+    if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token);
+      window.location.replace("/")
+    }
+    else{
+      alert(responseData.errors)
+    }
+  }
+  const signup = async () => {
+    console.log("Signup Function excuted",formData)
+    let responseData;
+    await fetch('http://localhost:4000/signup',{
+      method:'POST',
+      headers:{
+        Accept:'application/form-data',
+        'Content-type':'application/json'
+      },
+      body: JSON.stringify(formData),
+    }).then((response)=>response.json()).then((data)=>responseData=data)
+  
+    if(responseData.success){
+      localStorage.setItem('auth-token',responseData.token);
+      window.location.replace("/")
+    }
+    else{
+      alert(responseData.errors)
+    }
+  }
   return (
     <div className='loginsignup'>
       <div className="loginsignup-container">
-        <h1>Sign Up</h1>
+        <h1>{state}</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder="Your Name" />
-          <input type="email" placeholder="Email Address" />
-          <input type="password" placeholder="Password" />
+          {state==="Sign Up"?<input name='username' value={formData.username} onChange={changeHandler} type="text" placeholder="Your Name" />:<></>}
+          <input name='email' value={formData.email} onChange={changeHandler} type="email" placeholder="Email Address" />
+          <input name='password' value={formData.password} onChange={changeHandler} type="password" placeholder="Password" />
         </div>
-        <button>Continue</button>
-        <p className='loginsignup-login'>Already have a account? <span>Login here</span></p>
+        <button onClick={()=>(state==="Login"?login():signup())}>Continue</button>
+        {state==="Sign Up"
+          ?<p className='loginsignup-login'>Already have a account? <span onClick={()=>{setState("Login")}}>Login here</span></p>
+          :<p className='loginsignup-login'>Create an account? <span onClick={()=>{setState("Sign Up")}}>Click here</span></p>
+        }
         <div className="loginsignup-agree">
           <input type="checkbox" name = '' id ='' />
           <p>By continuing, I agre to the terms of use & praivacy policy.</p>
--- 2.backend/index.js
+++ 2.backend/index.js
@@ -122,6 +122,101 @@ app.get('/allproducts',async (req,res) => {
     res.send(products);
 })
 
+//Schema creating for user model
+const Users = mongoose.model('Users',{
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+//Creating Endpoint for register user
+app.post('/signup',async (req,res)=>{
+    let check = await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false,errors:"existing user found"})
+    }
+    let cart = {};
+    for (let i=0; i<300; i++){
+        cart[i]=0;
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    });
+    await user.save();
+
+const data = {
+    user:{
+        id:user.id
+    }
+}
+
+const token = jwt.sign(data,'secret.ecom');
+res.json({success:true,token})
+
+})
+
+//Creating endpoint for user login
+app.post('/login', async(req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if(user){
+        const passCompare = req.body.password === user.password;
+        if(passCompare){
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data, 'secret_ecom');
+            res.json({success:true, token});
+        }else{
+            res.json({success:false,errors:'Wrong Password'});
+        }
+    } else{
+        res.json({success:false, errors:'Wrong Email ID'})
+    }
+})
+
+// Creating endpoint for newcollection data
+
+app.get('/newcollections',async (req,res)=>{
+    let products = await Product.find({});
+    let newcollection = products.slice(1).slice(-8);
+    console.log('NewCollection Fetched');
+    res.send(newcollection);
+})
+
+// Creating endpoint for popular in women section
+app.get('/popularinwomen',async (req,res)=>{
+    let products = await Product.find({category:"women"});
+    let popular_in_women = products.slice(0,4);
+    console.log("Popular in women fetched");
+    res.send(popular_in_women)
+})
+
+// Creating endpoint for adding products in cart data
+
+app.post('/addtocart',async (req,res)=>{
+    console.log(req.body);
+})
+
+
 app.listen(port,(error) => {
     if(!error){
         console.log("Server Running on Port" + port)
